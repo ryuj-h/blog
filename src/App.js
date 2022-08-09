e08e4d5bf880d369e3237f0673a9085a61f914fd
ryuj-h <dbwlsgh57@naver.com>
2022-08-09 18:24:26 +0900

    two

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '강남 고기 맛집';
+  let styles = {color : 'blue', fontSize : '30px'};
+
+
+  function 함수(){
+    return 100;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요
-          //Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "black-nav">
+        <div style={styles}> asdfsadf </div>
+        <div> 개발 Blog</div>
+      </div>
+      <img src= {logo}/>
+      <h4>{ 함수() }</h4>
+      <h4>{ posts }</h4>
     </div>
   );
 }
