db2340a13d2209c1bfd9ece9f323b54a30badec8
ryuj-h <dbwlsgh57@naver.com>
2022-08-12 19:28:02 +0900

    #4 Update

--- README.md
+++ README.md
@@ -4,18 +4,20 @@
 
 # 기록
 
-1\
+1: 개발 환경 구축\
 https://www.notion.so/3-fb301feabc674e24bbd2316b13228743
 \
 \
-2\
+2: JSX 사용법\
 https://www.notion.so/3-fb301feabc674e24bbd2316b13228743
 \
 \
-3\
+3: useState를 사용하는 이유\
 https://www.notion.so/3-fb301feabc674e24bbd2316b13228743
-
-
+\
+\
+4: OnClick Event 구현\
+https://www.notion.so/4-3fa7df1b1bb941f89ab730d7980b7ad9
 
 
 # 튜토리얼_
--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -5,13 +7,19 @@ import './App.css';
 function App() {
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','강남 물난리']);
   //state를 사용하면 데이터가 변경되는 순간에 재 렌더링이 되어서 사용함.
+  let [따봉, 따봉변경] = useState(0);
+  
+
   return (
     <div className="App">
       <div className = "black-nav">
         <div>개발 Blog</div>
       </div>
+      <h3><span onClick={()=>{글제목변경(['여자 코트 추천','강남 우동 맛집','강남 물난리'])
+    }}><button>버튼</button></span></h3>
       <div className="list">
-        <h3>{글제목[0]}</h3>
+        <h3>{글제목[0]} <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> 
+        {따봉} </h3>
         <p>8월 10일 발행</p>
         <hr/>
         <h3>{글제목[1]}</h3>
